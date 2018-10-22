e921a1ed9c39775f018f5c5251864e731914cc47
elim322 <einer.lim322@gmail.com>
2018-10-21 19:57:09 -0700

    cleaning up styles

--- client/src/components/NavBar/styles.js
+++ client/src/components/NavBar/styles.js
@@ -1,32 +1,24 @@
 const styles = theme => ({
-  AppBar: {
-    background: '#f9a825',
-    display: 'flex',
-    position: 'relative',
-    height: '60px'
-  },
-  Toolbar: {
-    margin: '10px',
+  Tool: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between'
   },
+  AppBar: {
+    background: '#f9a825',
+    position: 'relative',
+    height: '60px'
+  },
+  Toolbar: {
+    margin: '10px'
+  },
   Logo: {
     height: '40px',
     width: 'auto',
-    position: 'relative',
-    bottom: '10px',
-    right: '10px'
-  },
-  Link: {
-    position: 'relative',
-    bottom: '10px'
-  },
-  Icon: {
-    margin: '10px'
+    alignItems: 'center'
   },
   Dots: {
-    opacity: '0.7'
+    opacity: '0.5'
   }
 });
 
