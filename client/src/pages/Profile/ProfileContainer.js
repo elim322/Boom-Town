225bbbfa10c01b0f375e33d146b3fa2aa9ca114a
elim322 <einer.lim322@gmail.com>
2018-10-29 12:42:25 -0700

    add profile container

---
+++ client/src/pages/Profile/ProfileContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import { ALL_USER_ITEMS_QUERY } from '../../apollo/queries';
+import Profile from './Profile';
+
+class ProfileContainer extends Component {
+  render() {
+    return (
+      <Query query={ALL_USER_ITEMS_QUERY} variables={{ filter: 1 }}>
+        {({ loading, error, data }) => {
+          if (loading) return loading;
+          if (error) return `${error}`;
+          if (data) {
+            console.log(data);
+            return <Profile data={data} />;
+          }
+        }}
+      </Query>
+    );
+  }
+}
+
+export default ProfileContainer;
